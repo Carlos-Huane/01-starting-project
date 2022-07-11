987be3395060229dd3baa38d558ddee7f6015442
Carlos-Huane <carloshuanesarmiento@gmail.com>
2022-07-10 19:31:59 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import {Route} from 'react-router-dom';
+import Products from './Pages/Products';
+import Welcome from './Pages/Welcome';
+import MainHeader from './Components/MainHeader';
+import ProductDetail from './Pages/ProductDetail';
+function App() {
+  return (
+    <div>
+      
+      <MainHeader />
+      <Route path="/products">
+        <Products />
+      </Route>
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
+      <Route path="/product-detail/:productId">
+        <ProductDetail />
+      </Route>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Products.js
@@ -0,0 +1,13 @@
+const Products = () => {
+    return (
+        <div>
+            <h1>The Products Page</h1>
+            <ul>
+                <li>A Book</li>
+                <li>A Carpet</li>
+                <li>A Course Online</li>
+            </ul>  
+        </div>
+    )
+}
+export default Products;
